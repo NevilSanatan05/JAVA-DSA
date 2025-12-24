e02934a5fb15e5dade5263baf7aac8922055c2ef
nevil811 <nevilbarick69@gmail.com>
2025-12-24 21:16:10 +0530

    Conditional Statements

---
+++ Conditional Statements & Loops/Conditions/conditional.cpp
@@ -0,0 +1,28 @@
+//If Else Conditions 
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int age;
+//     cout<<"Enter your age :";
+//     cin>>age;
+//     if (age>=18) {
+//         cout<<"You are eligible to vote.";
+//     } else if(age<18 && age>=10) {
+//         cout<<"You are not eligible to vote.";
+//     } else {
+//         cout<<"Invalid input.";
+//     }
+//     return 0;
+// }
+
+//Ternary Operator
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int age ;
+//     cout<<"Enter your age :";
+//     cin>>age;
+//     (age>=18)?cout<<"You are eligible to vote.":cout<<"You are not eligible to vote.";
+//     return 0;
+// }
+
