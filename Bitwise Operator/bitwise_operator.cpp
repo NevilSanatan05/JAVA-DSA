342fbc9e9d04773ab425010d7979266b588ce15d
nevil811 <nevilbarick69@gmail.com>
2025-12-27 21:26:08 +0530

    Bitwise - &,|,^,<<,>>

---
+++ Bitwise Operator/bitwise_operator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  // cout<<(4&8)<<endl; // Bitwise AND
+  // cout<<(4|8)<<endl;  // Bitwise OR
+  // cout<<(4^8)<<endl;  // Bitwise XOR
+  // cout<<(4<<1)<<endl; // Left Shift
+  // cout<<(10>>1)<<endl; // Right Shift
+  return 0;
+}
+
