8ac0a234494701df9474bd49a131ade210797ed2
nevil811 <nevilbarick69@gmail.com>
2026-01-17 12:01:33 +0530

    Scanner

--- Basics/InputDemo.java
+++ Basics/InputDemo.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class InputDemo{
   public static void main(String[] args){
-    Scanner sc= new Scanner(System.in);
-    double r = sc.nextInt();
-    double a = 3.142*r*r;
+    Scanner sc = new Scanner(System.in);
+    double r = sc.nextDouble();   // fixed
+    double a = 3.142 * r * r;
     System.out.println(a);
+    sc.close();
   }
 }
