d3d46af78b63eb82bcf954799446d641b09b10fa
nevil811 <nevilbarick69@gmail.com>
2026-01-17 22:10:08 +0530

    Increment & Decrement

---
+++ Basics/IncrementDecrement.java
@@ -0,0 +1,39 @@
+// public class IncrementDecrement {
+//   public static void main(String[] args) {
+//     int x = 10;
+//     System.out.println(x);
+//     x += 6;
+//     System.out.println(x);
+//     x -= 10;
+//     System.out.println(x);
+//   }
+// }
+
+
+// public class IncrementDecrement {
+//   public static void main(String[] args) {
+//     int x = 10;
+//     System.out.println(x++);
+//     System.out.println(x);
+//   }
+// }
+
+
+
+// public class IncrementDecrement {
+//   public static void main(String[] args) {
+//     int x = 10;
+//     System.out.println(++x);
+//     System.out.println(x);
+//   }
+// }
+
+
+
+public class IncrementDecrement {
+  public static void main(String[] args) {
+    int x = 10;
+    System.out.println(--x);
+    System.out.println(x);
+  }
+}
