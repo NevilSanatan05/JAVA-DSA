8f4f998436bc1951b59daabf01d8e5bceceacdb5
nevil811 <nevilbarick69@gmail.com>
2026-01-23 20:48:53 +0530

    Ternary Operator

---
+++ Conditions/Tern.java
@@ -0,0 +1,12 @@
+// import java.util.Scanner;
+
+// public class Tern {
+//   public static void main(String[] args) {
+//     Scanner ns = new Scanner(System.in);
+//     System.out.print("Enter your age : ");
+//     int age = ns.nextInt();
+//     System.out.println(age >= 18 ? "You can Vote" : "You Cannnot Vote");
+//     ns.close();
+//   }
+// }
+
