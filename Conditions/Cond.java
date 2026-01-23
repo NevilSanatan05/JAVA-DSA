192789ee9c513c082a5f55ada211b789b42a2ea8
nevil811 <nevilbarick69@gmail.com>
2026-01-23 20:20:33 +0530

    If and Else Conditions

---
+++ Conditions/Cond.java
@@ -0,0 +1,75 @@
+//Odd or even
+// import java.util.Scanner;
+// public class Cond{
+// public static void main(String[] args){
+// Scanner sc = new Scanner(System.in);
+// System.out.print("Enter a number : ");
+// int n = sc.nextInt();
+// if(n%2==0){
+// System.out.println("Number is even");
+// }
+// else{
+// System.out.println("Number is Odd");
+// }
+// sc.close();
+// }
+// }
+
+
+//Absolute 
+// import java.util.Scanner;
+// public class Cond {
+//   public static void main(String[] args) {
+//     Scanner ns = new Scanner(System.in);
+//     System.out.println("Enter a number : ");
+//     int n = ns.nextInt();
+//     if (n >= 0) {
+//       System.out.println(n);
+//     } else {
+//       System.out.println(-n);
+//     }
+//     ns.close();
+//   }
+// }
+
+
+//Is an integer or not
+// import java.util.Scanner;
+// public class Cond {
+//   public static void main(String[] args) {
+//     Scanner ns = new Scanner(System.in);
+//     System.out.println("Enter a number : ");
+//     double n = ns.nextDouble();
+//     int x = (int) n;
+//     if (n - x > 0) {
+//       System.out.println("not an integer");
+//     } else {
+//       System.out.println("Is an Integer");
+//     }
+//     ns.close();
+//   }
+// }
+
+
+//Profit or Loss
+// import java.util.Scanner;
+// public class Cond{
+//   public static void main(String[] args) {
+//     Scanner ns = new Scanner(System.in);
+//     System.out.print("Enter The Cost Price : ");
+//     double cp = ns.nextDouble();
+//     System.out.print("Enter the Selling Price :");
+//     double sp = ns.nextDouble();
+// if(sp>cp){
+//   System.out.println("Profit of "+ (sp-cp));
+// }
+// else if(cp>sp){
+//   System.out.println("Loss of : " +(cp-sp));
+// }
+// else{
+//   System.out.println("No Profit No Loss");
+// }
+// ns.close();
+//   }
+// }
+
