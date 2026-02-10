bbdef64e64d10716bccdb3df616788a6f410eecd
nevil811 <nevilbarick69@gmail.com>
2026-02-10 21:44:41 +0530

    Practice

---
+++ Java Programming Language/Prac.java
@@ -0,0 +1,28 @@
+// public class Prac {
+//   public static void main(String[] args) {
+//     int a=10,b=20,c=30;
+//     int avg = (a+b+c)/3;
+//     System.out.println(avg);
+//   }
+// }
+
+// import java.util.Scanner;
+
+// public class Prac {
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("Enter the side:");
+//     int side = sc.nextInt();
+//     int area = side * side;
+//     System.out.println("The area of the square is: " + area);
+//     sc.close();
+//   }
+// }
+
+
+public class Prac {
+  public static void main(String[] args) {
+   int $=24;
+   System.out.println($);
+  }
+}
