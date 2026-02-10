c61b3281d37366e5f5841a9a89144f30f874a50f
nevil811 <nevilbarick69@gmail.com>
2026-02-10 22:06:40 +0530

    Operator

---
+++ Operators/Oper.java
@@ -0,0 +1,65 @@
+// public class Oper{
+//   public static void main(String[] args) {
+//     int a = 10;
+//     int b = 5;
+//     System.out.println("Addition: " + (a + b));
+//     System.out.println("Subtraction: " + (a - b));
+//     System.out.println("Multiplication: " + (a * b));
+//     System.out.println("Division: " + (a / b));
+//     System.out.println("Modulo: " + (a % b));
+
+//   }
+// }
+
+//public class Oper{
+ // public static void main(String[] args) {
+    // int a = 10;
+    // int b = ++a;
+    // System.out.println(a);
+    // System.out.println(b);
+    
+    // int a = 10;
+    // int b = a++;
+    // System.out.println(a);
+    // System.out.println(b);
+
+    // int a = 10;
+    // int b = --a;
+    // System.out.println(a);
+    // System.out.println(b);
+
+    // int a = 10;
+    // int b = a--;
+    // System.out.println(a);
+    // System.out.println(b);
+    
+
+ // }
+//}
+
+
+// public class Oper{
+//   public static void main(String[] args) {
+//     int a = 10;
+//     int b = 5;
+//     System.out.println("a > b: " + (a > b));
+//     System.out.println("a < b: " + (a < b));
+//     System.out.println("a >= b: " + (a >= b));
+//     System.out.println("a <= b: " + (a <= b));
+//     System.out.println("a == b: " + (a == b));
+//     System.out.println("a != b: " + (a != b));
+
+//   }
+// }
+
+
+public class Oper{
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 5;
+    System.out.println("a > b && a != b: " + (a > b && a != b));
+    System.out.println("a < b || a == b: " + (a < b || a == b));
+    System.out.println("!(a > b): " + !(a > b));
+
+  }
+}
