79a2a66668e208907df35742a704d6cd2cad3480
nevil811 <nevilbarick69@gmail.com>
2026-02-11 12:22:06 +0530

    Operator

--- Operators/Oper.java
+++ Operators/Oper.java
@@ -53,13 +53,36 @@
 // }
 
 
-public class Oper{
-  public static void main(String[] args) {
-    int a = 10;
-    int b = 5;
-    System.out.println("a > b && a != b: " + (a > b && a != b));
-    System.out.println("a < b || a == b: " + (a < b || a == b));
-    System.out.println("!(a > b): " + !(a > b));
+// public class Oper{
+//   public static void main(String[] args) {
+//     int a = 10;
+//     int b = 5;
+//     System.out.println("a > b && a != b: " + (a > b && a != b));
+//     System.out.println("a < b || a == b: " + (a < b || a == b));
+//     System.out.println("!(a > b): " + !(a > b));
 
-  }
-}
+//   }
+// }
+
+
+// public class Oper{
+//   public static void main(String[] args) {
+//    //Assignment operator
+//     int a = 10; 
+//     a += 5; 
+//     System.out.println("a after += 5: " + a);
+    
+//     a -= 3; 
+//     System.out.println("a after -= 3: " + a);
+    
+//     a *= 2; 
+//     System.out.println("a after *= 2: " + a);
+    
+//     a /= 4; 
+//     System.out.println("a after /= 4: " + a);
+    
+//     a %= 3; 
+//     System.out.println("a after %= 3: " + a);
+
+//   }
+// }
