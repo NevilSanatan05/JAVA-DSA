e630b184528f3797ac9286bd5b82d96de29a3107
nevil811 <nevilbarick69@gmail.com>
2026-02-12 20:23:38 +0530

    For Loop

--- Loops/for_loop.java
+++ Loops/for_loop.java
@@ -1,7 +1,29 @@
+// public class for_loop {
+//   public static void main(String[] args) {
+//     for(int i =0;i<=10;i++){
+//       System.out.println(i);
+//     }
+//   }
+// }
+
+
+// public class for_loop {
+//   public static void main(String[] args) {
+//    for(int i =0;i<=3;i++){
+//      System.out.println("****");
+//    }
+//   }
+// }
+
+//Reverse of a number
 public class for_loop {
   public static void main(String[] args) {
-    for(int i =0;i<=10;i++){
-      System.out.println(i);
+    int n = 134;
+    int rev = 0;
+    for(int i = n;i>0;i/=10){
+      int rem = i%10;
+      rev = rev*10+rem;
     }
+    System.out.println(rev);
   }
 }
