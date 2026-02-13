bd9f1ae804d8ceca83d6a3ba525c48d821bdbc45
nevil811 <nevilbarick69@gmail.com>
2026-02-13 21:46:15 +0530

    Loops Practice Question

--- Loops/Prac.java
+++ Loops/Prac.java
@@ -65,16 +65,28 @@
 //   }
 // }
 
-import java.util.Scanner;
-public class Prac{
-public static void main(String[] args) {
-   Scanner sc = new Scanner(System.in);
-   System.out.print("Enter a number: "); 
-   int n = sc.nextInt(); 
-   for(int i=1;i<=10;i++){
-    System.out.println(n+"x"+i+"="+(i*n));
-  }
-  sc.close();
-   }
+// import java.util.Scanner;
+// public class Prac{
+// public static void main(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//    System.out.print("Enter a number: "); 
+//    int n = sc.nextInt(); 
+//    for(int i=1;i<=10;i++){
+//     System.out.println(n+"x"+i+"="+(i*n));
+//   }
+//   sc.close();
+//    }
    
+// }
+
+
+
+public class Prac {
+  public static void main(String[] args) {
+    int i = 0;
+    while (i < 5) {
+      System.out.println("Hello");
+      i += 2;
+    }
+  }
 }
