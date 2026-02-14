7b7612e6161ff2dc2cf29b0c7d4210e73ba5d733
nevil811 <nevilbarick69@gmail.com>
2026-02-14 21:03:56 +0530

    Patterns

---
+++ Patterns/Pattern_1.java
@@ -0,0 +1,56 @@
+//Star Pattern
+// public class Pattern_1 {
+
+//     public static void main(String[] args) {
+//       for(int i=1;i<=4;i++){
+//         for(int j=1;j<=i;j++){
+//           System.out.print("*");
+//         }
+//         System.out.println("");
+//       }
+//     }
+// }
+
+//Inverted Star Pattern
+// public class Pattern_1 {
+
+//     public static void main(String[] args) {
+//       int n =4;
+//       for(int i=1;i<=n;i++){
+//         for(int j = 1;j<=n-i+1;j++){
+//           System.err.print("*");
+//         }
+//          System.out.println("");
+//       }
+
+//     }
+// }
+
+//Half Pyramid Pattern
+// public class Pattern_1 {
+
+//   public static void main(String[] args) {
+//     for (int i = 1; i <= 4; i++) {
+//       for (int j = 1; j <= i; j++) {
+//         System.out.print(j);
+//       }
+//       System.out.println("");
+//     }
+//   }
+
+// }
+
+//Character Pattern
+// public class Pattern_1 {
+//   public static void main(String[] args) {
+//     char ch = 'A';
+//     for (int i = 1; i <= 4; i++) {
+
+//       for (int j = 1; j <= i; j++) {
+//         System.out.print(ch);
+//         ch++;
+//       }
+//       System.out.println("");
+//     }
+//   }
+// }
