7de96f19b1ebaf020020d17fac1a21931994e46a
nevil811 <nevilbarick69@gmail.com>
2026-02-16 20:43:19 +0530

    Functions Syntax and Parameters / Arguments

---
+++ Functions & Methods/JavaBasics.java
@@ -0,0 +1,56 @@
+// public class JavaBasics {
+//   public static void printHelloWorld() {
+//     System.out.println("Hello World!");
+//   }
+
+//   public static void main(String[] args) {
+//     printHelloWorld();
+//   }
+// }
+
+// public class JavaBasics {
+//   public static void printHelloWorld() {
+//     System.out.println("Hello World!");
+//     return;
+//   }
+
+//   public static void main(String[] args) {
+//     printHelloWorld();
+//   }
+// }
+
+//Functions with parameters and return type
+
+// public class JavaBasics {
+
+// public static int Sum(int a,int b){
+// return a+b;
+// }
+//   public static void main(String[] args) {
+//  System.out.println(Sum(3,4));
+//   }
+// }
+
+// import java.util.Scanner;
+
+// public class JavaBasics {
+
+//   public static int Sum() {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("Enter the value of a and b : ");
+//     int a = sc.nextInt();
+//     int b = sc.nextInt();
+//     int Sum =a+b;
+//     return Sum;
+//   }
+
+//   public static void main(String[] args) {
+//     System.out.println(Sum());
+//   }
+// }
+
+
+//parameters - values that we pass to a function when we call it. They are used to provide input to the function and can be of any data type, such as int, String, etc.
+//arguments - values that we pass to a function when we call it. They are used to provide input to the function and can be of any data type, such as int, String, etc. The terms "parameters" and "arguments" are often used interchangeably, but technically, parameters refer to the variables defined in the function signature, while arguments refer to the actual values passed to the function when it is called.
+
+
