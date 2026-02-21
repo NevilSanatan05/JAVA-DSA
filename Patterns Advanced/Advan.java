8c97f6b524c0df36184cfa86439b8c613ea75638
nevil811 <nevilbarick69@gmail.com>
2026-02-21 20:46:13 +0530

    Hollow , inverted & rotated pattern

--- Patterns Advanced/Advan.java
+++ Patterns Advanced/Advan.java
@@ -1,7 +1,55 @@
-public class Advan {
-  public static void main(String[] args) {
-    for(int i=1;i<=5;i++){
-      System.out.println("******");
-    }
-  }
-}
+// public class Advan {
+//   public static void hollow_rectangle(int totRows , int totCols){
+//     for(int i=1;i<=totRows;i++){
+//       for(int j=1;j<=totCols;j++){
+//         if(i==1||i==totRows||j==1||j==totCols){
+//           System.out.print("*");
+//         }
+//         else{
+//           System.out.print(" ");
+//         }
+//       }
+//       System.out.println();
+//     }
+//   }
+//   public static void main(String[] args) {
+//     hollow_rectangle(4, 5);
+//   }
+// } 
+
+
+//Inverted and rotated half pyramid
+// public class Advan {
+// public static void inverted_rotated_half_pyramid(int totRows){
+//   for(int i=1;i<=totRows;i++){
+//     for(int j=1;j<=totRows-i;j++){
+//       System.out.print(" ");
+
+//     }
+//     for(int k=1;k<=i;k++){
+//       System.out.print("*");
+//     }
+//     System.out.println("");
+//   }
+// }
+//   public static void main(String[] args) {
+//     inverted_rotated_half_pyramid(4); 
+//   }
+// }
+
+
+//Inverted half pyramid with numbers
+// public class Advan {
+//   public static void inverted_half_pyramid(int totRows) {
+//     for (int i = 1; i <= totRows; i++) {
+//       for (int j = 1; j <= totRows - i + 1; j++) {
+//         System.out.print(j + " ");
+//       }
+//       System.out.println("");
+//     }
+//   }
+
+//   public static void main(String[] args) {
+//     inverted_half_pyramid(4);
+//   }
+// }
