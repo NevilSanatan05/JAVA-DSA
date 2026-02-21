940c724c6de0bab582f9558674ee5f3c298c2e67
nevil811 <nevilbarick69@gmail.com>
2026-02-21 21:11:58 +0530

    Advanced Patterns

--- Patterns Advanced/Advan.java
+++ Patterns Advanced/Advan.java
@@ -17,7 +17,6 @@
 //   }
 // } 
 
-
 //Inverted and rotated half pyramid
 // public class Advan {
 // public static void inverted_rotated_half_pyramid(int totRows){
@@ -37,7 +36,6 @@
 //   }
 // }
 
-
 //Inverted half pyramid with numbers
 // public class Advan {
 //   public static void inverted_half_pyramid(int totRows) {
@@ -53,3 +51,42 @@
 //     inverted_half_pyramid(4);
 //   }
 // }
+
+//Floyd's triangle
+// public class Advan {
+//   public static void floyds_triangle(int totRows) {
+//     int num = 1;
+//     for (int i = 1; i <= totRows; i++) {
+//       for (int j = 1; j <= i; j++) {
+//         System.out.print(num + " ");
+//         num++;
+//       }
+//       System.out.println("");
+//     }
+//   }
+//   public static void main(String[] args) {
+//     floyds_triangle(5);
+//   }
+//   }
+
+//0-1 triangle
+// public class Advan {
+// public static void zero_one_triangle(int totRows) {
+//   for (int i = 1; i <= totRows; i++) {            
+//     for (int j = 1; j <= i; j++) {
+//       if ((i + j) % 2 == 0) {
+//         System.out.print("1 ");
+//       } else {
+//         System.out.print("0 ");
+//       }
+      
+//     }
+//     System.out.println("");
+//   }
+// }
+
+//   public static void main(String[] args) {
+//     zero_one_triangle(5);
+//   }
+//}
+
