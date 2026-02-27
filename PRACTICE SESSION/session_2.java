ad3d4fb5aeec0767df00f658a7e4444984d32b89
nevil811 <nevilbarick69@gmail.com>
2026-02-27 22:24:29 +0530

    Practice Session -2

---
+++ PRACTICE SESSION/session_2.java
@@ -0,0 +1,49 @@
+// public class session_2 {
+
+//   public static void main(String[] args) {
+//     int n =4;
+//     for(int i=1;i<=n;i++){
+//       for(int j=1;j<=n;j++){
+//         System.out.print("*");
+//       }
+//       System.out.println();
+//     }
+//   }
+// }
+
+// public class session_2 {
+//  public static void main(String[] args) {
+//   int n = 5;
+//   for(int i=1;i<=n;i++){
+//     //print spaces
+//     for(int j = 1;j<=n-i;j++){
+//       System.out.print(" ");
+//     }
+//     //print stars
+//     for(int k=1;k<=i;k++){
+//       System.out.print("*");
+//     }
+//     System.out.println();
+//   }
+//  }
+// }
+
+// public class session_2 {
+//   public static void main(String[] args) {
+//     int n = 5;
+//     for (int i = 1; i <= n; i++) {
+//       for (int j = 1; j <= i; j++) {
+//         if (j <= i) {
+//           System.out.print("*");
+//         } else {
+//           System.out.print("");
+//         }
+
+//       }
+//       System.out.println();
+//     }
+//   }
+
+// }
+
+//Upper Left Triangle
