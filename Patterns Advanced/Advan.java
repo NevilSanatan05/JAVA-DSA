00a3f166094313af6d5604a8ae38ce4fc2fe18a8
nevil811 <nevilbarick69@gmail.com>
2026-02-27 11:55:12 +0530

    Advanced Patterns

--- Patterns Advanced/Advan.java
+++ Patterns Advanced/Advan.java
@@ -79,7 +79,7 @@
 //       } else {
 //         System.out.print("0 ");
 //       }
-      
+
 //     }
 //     System.out.println("");
 //   }
@@ -90,3 +90,131 @@
 //   }
 //}
 
+//Butterfly pattern
+// public class Advan {
+//   public static void butterfly(int n) {
+//     // 1st half
+//     // stars = i
+//     for (int i = 1; i <= n; i++) {
+//       for (int j = 1; j <= i; j++) {
+//         System.out.print("*");
+//       }
+
+//       // spaces = 2*(n-i)
+//       for (int j = 1; j <= 2 * (n - i); j++) {
+//         System.out.print(" ");
+//       }
+
+//       // stars = i
+//       for (int j = 1; j <= i; j++) {
+//         System.out.print("*");
+//       }
+//       System.out.println(" ");
+//     }
+
+//     // 2nd half
+//     // stars = i
+//     for (int i = n - 1; i >= 1; i--) {
+//       for (int j = 1; j <= i; j++) {
+//         System.out.print("*");
+//       }
+
+//       // spaces = 2*(n-i)
+//       for (int j = 1; j <= 2 * (n - i); j++) {
+//         System.out.print(" ");
+//       }
+
+//       // stars = i
+//       for (int j = 1; j <= i; j++) {
+//         System.out.print("*");
+//       }
+//       System.out.println("");
+//     }
+
+//   }
+
+//   public static void main(String[] args) {
+//     butterfly(5);
+//   }
+// }
+
+
+//Solid rhombus
+// public class Advan{
+//   public static void solid_rhombus(int n){
+//     for(int i=1;i<=n;i++){
+//       //spaces
+//       for(int j=1;j<=n-i;j++){
+//         System.out.print(" ");
+//       }
+//       //stars
+//       for(int j=1;j<=n;j++){
+//         System.out.print("*");
+//       }
+//       System.out.println("");
+//     }
+//   }
+//   public static void main(String[] args) {
+//     solid_rhombus(5);
+//   }
+// }
+
+//Hollow rhombus
+// public class Advan{
+//   public static void hollow_rhombus(int n){
+//     for(int i=1;i<=n;i++){
+//       //spaces
+//       for(int j=1;j<=n-i;j++){
+//         System.out.print(" ");
+//       }
+//       //stars
+//       for(int j=1;j<=n;j++){
+//         if(i==1||i==n||j==1||j==n){
+//           System.out.print("*");
+//         }
+//         else{
+//           System.out.print(" ");
+//         }
+//       }
+//       System.out.println("");
+//     }
+//   }
+//   public static void main(String[] args) {
+//     hollow_rhombus(5);
+//   }
+// }
+
+
+//Diamond pattern
+// public class Advan{
+//   public static void diamond(int n){
+//     //1st half
+//     for(int i=1;i<=n;i++){
+//       //spaces
+//       for(int j=1;j<=n-i;j++){
+//         System.out.print(" ");
+//       }
+//       //stars
+//       for(int j=1;j<=2*i-1;j++){
+//         System.out.print("*");
+//       }
+//       System.out.println("");
+//     }
+
+//     //2nd half
+//     for(int i=n-1;i>=1;i--){
+//       //spaces
+//       for(int j=1;j<=n-i;j++){
+//         System.out.print(" ");
+//       }
+//       //stars
+//       for(int j=1;j<=2*i-1;j++){
+//         System.out.print("*");
+//       }
+//       System.out.println("");
+//     }
+//   }
+//   public static void main(String[] args) {
+//     diamond(4);
+//   }
+// }
