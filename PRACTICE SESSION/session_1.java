934108e83e5f34af0e4c99f8606037708065988d
nevil811 <nevilbarick69@gmail.com>
2026-02-27 21:53:02 +0530

    Practice Session -1

--- PRACTICE SESSION/session_1.java
+++ PRACTICE SESSION/session_1.java
@@ -1,5 +1,89 @@
-public class session_1 {
-  public static void main(String[] args) {
-    System.out.println("Hello");
-  }
-}
+//Loops
+
+// public class session_1 {
+//   public static void main(String[] args) {
+//     for (int i = 0; i < 5; i++) {
+//       System.out.println(i);
+//     }
+//   }
+// }
+
+// public class session_1 {
+//  public static void main(String[] args) {
+//   int i =0;
+//   while(i<5){
+//     System.out.println(i);
+//     i++;
+//   }
+//  }
+// }
+
+// public class session_1 {
+//  public static void main(String[] args) {
+//   int i =0;
+//   do{
+//     System.out.println(i);
+//     i++;
+//   } while(i<5);
+//  }
+// }
+
+//Question - 1
+// public class session_1 {
+//  public static void main(String[] args) {
+//   for(int i=0;i<5;i++){
+//     System.out.println("Hello");
+//     i+=2;
+//   }
+//  }
+// }
+
+//Question - 2
+// import java.util.Scanner;
+
+// public class session_1 {
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("How many integers to read : ");
+//     int n = sc.nextInt();
+//     int odd = 0, even = 0;
+//     for (int i = 1; i <= n; i++) {
+//       System.out.print("Enter a value : ");
+//       int num = sc.nextInt();
+//       if (num % 2 == 0) {
+//         even += num;
+//       } else {
+//         odd += num;
+//       }
+
+//     }
+//     System.out.println("Odd Sum = " + odd);
+//     System.out.println("Even Sum = " + even);
+//   }
+// }
+
+
+//Question - 3
+// public class session_1 {
+//   public static void main(String[] args) {
+//     int n = 2;
+//     int sum = 0;
+//     for (int i = 1; i <= n; i++) {
+//       sum += i;
+//     }
+//     System.out.println("The Factorial of "+ n + " is : " +sum);
+//   }
+// }
+
+
+//Question - 4 
+// public class session_1 {
+//  public static void main(String[] args) {
+//   int n = 4;
+//   for(int i=1;i<=10;i++){
+//     System.out.println(n + "X" +i+" = "+n*i);
+//   }
+//  }
+// }
+
+
