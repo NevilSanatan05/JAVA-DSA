16d3dabc733997f32780c8d28979c8f6a2757db4
nevil811 <nevilbarick69@gmail.com>
2026-03-07 20:49:32 +0530

    Revision - Pseudocodes & DataTypes

---
+++ REVISION/basic.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class basic {
+  public static void main(String[] args) {
+    // int a = 10,b=20;
+    Scanner sc = new Scanner(System.in);
+    // System.out.print("Enter the value of a : ");
+    // int a = sc.nextInt();
+    // System.out.print("Enter the value of b : ");
+
+    // int b = sc.nextInt();
+    // System.out.println("The Sum of two numbers is : " + (a + b));
+    // System.out.println("Hello World");
+
+    long a = 25;
+    int b = (int) a;
+
+    char ch = 'A';
+    // System.out.println(ch);
+    int c = ch;
+    // System.out.println(c);
+    // System.out.println(b);
+    sc.close();
+  }
+}
