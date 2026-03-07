646d678561fd579e39df471959c81af52ba97bc5
nevil811 <nevilbarick69@gmail.com>
2026-03-07 20:56:18 +0530

    Revision - Pseudocodes & DataTypes

---
+++ REVISION/operator_conditional.java
@@ -0,0 +1,16 @@
+public class operator_conditional {
+  public static void main(String[] args) {
+    int a = 10;
+    // int b = 20;
+    // int c = ++a;
+    // int d = a++;
+    // int e = --a;
+    int f = a--;
+    // System.out.println(a+b);
+    System.out.println(a);
+    // System.out.println(c);
+    // System.out.println(d);
+    // System.out.println(e);
+    System.out.println(f);
+  }
+}
