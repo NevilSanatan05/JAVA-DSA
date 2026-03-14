7966a7110c1a7ed37d4dd3e6e1a363fae3314c3c
nevil811 <nevilbarick69@gmail.com>
2026-03-14 12:12:11 +0530

    Substring

--- STRINGS/Basic.java
+++ STRINGS/Basic.java
@@ -31,3 +31,47 @@
 //     printLetters(lastName);
 //   }
 // }
+
+//Comparing
+// public class Basic {
+//   public static void main(String[] args) {
+//     String s1 = "Tony";
+//     String s2 = "Tony";
+//     String s3 = new String("Tony");
+
+//     // if (s1 == s2) {
+//     //   System.out.println("Strings are equal");
+//     // } else {
+//     //   System.out.println("Strings are not equal");
+//     // }
+
+//     // if (s1 == s3) {
+//     //   System.out.println("Strings are equal");
+//     // } else {
+//     //   System.out.println("Strings are not Equal");
+//     // }
+
+//     if(s1.equals(s3)){
+// System.out.println("Strings are equal");
+//     }
+//     else{
+//       System.out.println("Strings are not equal");
+//     }
+//   }
+// }
+
+
+//String Functions - SubString
+public class Basic {
+  public static String substring(String str,int si,int ei) {
+    String substr = "";
+    for(int i=si;i<ei;i++){
+      substr += str.charAt(i);
+    }
+    return substr;
+  }
+ public static void main(String[] args) {
+  String str = "HelloWorld";
+  System.out.println(substring(str,0,5));
+ }
+}
