9f35bcda555f481f272b37f34feb4d3ebf67a82a
nevil811 <nevilbarick69@gmail.com>
2026-03-17 12:01:56 +0530

    Strings

--- STRINGS/Question.java
+++ STRINGS/Question.java
@@ -61,3 +61,40 @@
 //         System.out.println("Shortest Distance : " + result);
 //     }
 // }
+
+
+//Largest String
+// public class Question{
+//   public static void main(String[] args) {
+//     String fruits[] = {"apple","mango","banana"};
+//     String largest = fruits[0];
+//     for(int i=1;i<fruits.length;i++){
+//       if(largest.compareTo(fruits[i])<0){
+//         largest=fruits[i];
+//       }
+//     }
+//     System.out.println(largest);
+//   }
+// }
+
+
+//Strings are immutable
+
+// public class Question{
+//   public static void main(String[] args) {
+//     StringBuilder sb = new StringBuilder("");
+//     for(char ch='a';ch<='z';ch++){
+//       sb.append(ch);
+//     }
+//     System.out.println(sb);
+//   }
+// }
+
+
+// public class Question{
+//   public static void main(String[] args) {
+//     String s1 = "Hi , i am nevil";
+//     String s2 = s1.;
+//     System.out.println(s2);
+//   }
+// }
