2f7a863e37725aea52b2a93abd21b94176231695
nevil811 <nevilbarick69@gmail.com>
2026-03-18 13:11:23 +0530

    Bit Operators

---
+++ BIT MANIPULATION/operator.java
@@ -0,0 +1,10 @@
+public class operator {
+  public static void main(String[] args) {
+    System.out.println(5&6);  //AND
+    System.out.println(5|6); //OR
+    System.out.println(5^6); // ZOR 
+    System.out.println((~5)); // One complement
+    System.out.println(5<<2);
+    System.out.println(5>>2);
+  }
+}
