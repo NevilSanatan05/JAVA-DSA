808ae89318b95aae113d6b1a718aed4a52d1d916
nevil811 <nevilbarick69@gmail.com>
2026-03-19 12:16:37 +0530

    Bit Manipulation

---
+++ BIT MANIPULATION/ith.java
@@ -0,0 +1,32 @@
+//Get ith bit 
+// public class ith {
+//   public static int getIthBit(int n , int i){
+//     int bitMask = 1<<i;
+//     if((n & bitMask) == 0){
+//       return 0;
+//     }
+//     else{
+//       return 1;
+//     }
+//   }
+//   public static void main(String[] args) {
+//     System.out.println(getIthBit(10, 2));
+//   }
+// }
+
+
+
+//Set ith bit
+// public class ith {
+//   public static int getIthBit(int n , int i){
+//     int bitMask = 1<<i;
+//     return n | bitMask;
+//   }
+//   public static void main(String[] args) {
+//     System.out.println(getIthBit(10, 2));
+//   }
+// }
+
+
+
+
