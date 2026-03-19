cf9c99a2cd52193c5eaf4ab7ce1846e9b336c312
nevil811 <nevilbarick69@gmail.com>
2026-03-19 11:43:49 +0530

    Bit Operators

--- BIT MANIPULATION/operator.java
+++ BIT MANIPULATION/operator.java
@@ -1,28 +1,28 @@
-// public class operator {
+// // public class operator {
+// //   public static void main(String[] args) {
+// //     System.out.println(5&6);  //AND
+// //     System.out.println(5|6); //OR
+// //     System.out.println(5^6); // ZOR 
+// //     System.out.println((~5)); // One complement
+// //     System.out.println(5<<2);
+// //     System.out.println(5>>2);
+// //   }
+// // }
+
+// public class Operator {
+
+//   public static void oddOrEven(int n) {
+//     int bitMask = 1;
+//     if ((n & bitMask) == 0) {
+//       System.out.println("Even number");
+//     } else {
+//       System.out.println("Odd number");
+//     }
+//   }
+
 //   public static void main(String[] args) {
-//     System.out.println(5&6);  //AND
-//     System.out.println(5|6); //OR
-//     System.out.println(5^6); // ZOR 
-//     System.out.println((~5)); // One complement
-//     System.out.println(5<<2);
-//     System.out.println(5>>2);
+//     oddOrEven(3);
+//     oddOrEven(11);
+//     oddOrEven(14);
 //   }
 // }
-
-public class Operator {
-
-  public static void oddOrEven(int n) {
-    int bitMask = 1;
-    if ((n & bitMask) == 0) {
-      System.out.println("Even number");
-    } else {
-      System.out.println("Odd number");
-    }
-  }
-
-  public static void main(String[] args) {
-    oddOrEven(3);
-    oddOrEven(11);
-    oddOrEven(14);
-  }
-}
