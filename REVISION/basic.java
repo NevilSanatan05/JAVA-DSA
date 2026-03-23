d22bca1f0e9eb2372dc26ee3e0a4e53d6fa45d76
nevil811 <nevilbarick69@gmail.com>
2026-03-23 11:17:01 +0530

    Revision

--- REVISION/basic.java
+++ REVISION/basic.java
@@ -23,3 +23,14 @@
 //     sc.close();
 //   }
 // }
+
+public class basic {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 4; i++) {
+      for (int j = 1; j <= 4; j++) {
+        System.out.print("*");
+      }
+      System.out.println("");
+    }
+  }
+}
