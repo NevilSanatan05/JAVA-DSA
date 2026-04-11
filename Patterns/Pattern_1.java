9a6da0751537c9b57b60e4c7e1da20a2396218d5
nevil811 <nevilbarick69@gmail.com>
2026-04-11 21:23:00 +0530

    Patterns

--- Patterns/Pattern_1.java
+++ Patterns/Pattern_1.java
@@ -41,16 +41,16 @@
 // }
 
 //Character Pattern
-// public class Pattern_1 {
-//   public static void main(String[] args) {
-//     char ch = 'A';
-//     for (int i = 1; i <= 4; i++) {
+public class Pattern_1 {
+  public static void main(String[] args) {
+    char ch = 'A';
+    for (int i = 1; i <= 4; i++) {
 
-//       for (int j = 1; j <= i; j++) {
-//         System.out.print(ch);
-//         ch++;
-//       }
-//       System.out.println("");
-//     }
-//   }
-// }
+      for (int j = 1; j <= i; j++) {
+        System.out.print(ch);
+        ch++;
+      }
+      System.out.println("");
+    }
+  }
+}
