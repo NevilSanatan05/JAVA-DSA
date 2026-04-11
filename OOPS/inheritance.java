9e188e18484272fa1000b8771e4a18fdd1512467
nevil811 <nevilbarick69@gmail.com>
2026-04-11 20:17:41 +0530

    OOPS

--- OOPS/inheritance.java
+++ OOPS/inheritance.java
@@ -1,36 +1,36 @@
-public class inheritance {
-  public static void main(String[] args) {
-    Dog dobby = new Dog();
-    dobby.eat();
-    dobby.legs = 4;
-    System.out.println(dobby.legs);
-  }
-}
-
-//Base Class
-class Animal{
-  String color;
-  void eat(){
-    System.out.println("eats");
-  }
-  void breathe(){
-    System.out.println("breathes");
-  }
-}
-
-class Mammals extends Animal{
-  int legs;
-}
-class Dog extends Mammals{
-  String bread;
-}
-// //Derived Class
-// class Fish extends Animal{
-//   int fins;
-//   void swim(){
-//     System.out.println("swims in water..");
+// public class inheritance {
+//   public static void main(String[] args) {
+//     Dog dobby = new Dog();
+//     dobby.eat();
+//     dobby.legs = 4;
+//     System.out.println(dobby.legs);
 //   }
 // }
 
+// //Base Class
+// class Animal{
+//   String color;
+//   void eat(){
+//     System.out.println("eats");
+//   }
+//   void breathe(){
+//     System.out.println("breathes");
+//   }
+// }
 
-//Types of Inheritance
+// class Mammals extends Animal{
+//   int legs;
+// }
+// class Dog extends Mammals{
+//   String bread;
+// }
+// // //Derived Class
+// // class Fish extends Animal{
+// //   int fins;
+// //   void swim(){
+// //     System.out.println("swims in water..");
+// //   }
+// // }
+
+
+// //Types of Inheritance
